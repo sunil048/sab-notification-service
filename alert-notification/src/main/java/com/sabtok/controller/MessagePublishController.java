ae5197a7d6dff457c936148a59cf58b64f31e8f8
Sunil <mk.karbasappa@gmail.com>
2026-04-17 22:34:08 +0530

    URS-60049 : added producer

--- alert-notification/src/main/java/com/sabtok/controller/MessagePublishController.java
+++ alert-notification/src/main/java/com/sabtok/controller/MessagePublishController.java
@@ -1,17 +1,42 @@
 package com.sabtok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+
 @RestController
 @RequestMapping("/notification")
 public class MessagePublishController {
 
+    private static final String TOPIC = "sab-event";
+    private static final String KEY = "Log";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
     @PostMapping("/send")
     public String postMessage(@RequestBody String message) {
-        System.out.println(message);
-        return message;
+
+        CompletableFuture<SendResult<String, String>> future
+                = this.kafkaTemplate.send(TOPIC, KEY, message);
+
+        final AtomicReference<String> response = new AtomicReference<>();
+        future.whenComplete((result, throwable) -> {
+            if (throwable != null) {
+                //logger.error("Unable to send message : " + message, throwable);
+                response.set("Unable to send message : " + message);
+            } else {
+                //logger.info("Sent message: " + message + " with offset: " + result.getRecordMetadata().offset());
+                response.set("Sent message: " + message + " with offset: " + result.getRecordMetadata().offset());
+            }
+        });
+        return response.get();
     }
 }
